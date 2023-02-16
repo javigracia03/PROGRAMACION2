51fb0ee7da403b2317012a12d530e2b6b6e0e1d3
Javier Gracia <javiergracia@MacBook-Pro-de-Javier.local>
2023-02-16 12:25:02 +0100

    Primera practica terminada

--- PROGRAMACION2_GTI/PRACTICA1/mainPorTres3.js
+++ PROGRAMACION2_GTI/PRACTICA1/mainPorTres3.js
@@ -1,3 +1,35 @@
+
+
+
+// R -> porTres() -> R
+function porTres1( a ) {
+    return a * 3;
+}
+
+function porTresA( a, retornar ) {
+    setTimeout( function() {
+        // han pasado 4 segundos
+        retornar( a * 3 ) // devolver a * 3
+    }, 4000)
+}
+
+
+
+// main
+
+var r = porTres1( 5 )
+console.log( r )
+
+porTresA( 5, function( res ) {
+    console.log( res )
+    
+})
+
+
+
+
+/*
+
 function porTres(a){
     return new Promise ( resolve => {
         setTimeout(() => {
@@ -9,6 +41,9 @@ function porTres(a){
 
 
 
+
+
+
 async function pruebaPorTres(){
     const result = await porTres(4);
     if (result === 12){
@@ -17,3 +52,5 @@ async function pruebaPorTres(){
 }
 
 pruebaPorTres();
+
+*/
