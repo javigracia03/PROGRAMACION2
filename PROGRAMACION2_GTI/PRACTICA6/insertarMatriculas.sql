6e1d846256e5880d88414e013ccb8280053d9cbe
Javier Gracia <javiergracia@MacBook-Pro-de-Javier.local>
2023-02-18 21:11:38 +0100

    first two tables created

---
+++ PROGRAMACION2_GTI/PRACTICA6/insertarMatriculas.sql
@@ -0,0 +1,4 @@
+insert into Matricula values('20123456A','13928');
+insert into Matricula values('20123456A','13929');
+insert into Matricula values('20123457B','13928');
+insert into Matricula values('20123457B','13929');
